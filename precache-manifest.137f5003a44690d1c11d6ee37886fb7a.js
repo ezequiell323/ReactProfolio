eeb8c703b03cb90274c62391ca17e31d87889c07
Eze <eze.largaespada@gmail.com>
2020-06-08 23:09:20 -0400

    Updates

---
+++ precache-manifest.137f5003a44690d1c11d6ee37886fb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695a9554d1a140cb5d2cbf8bce0c6da7",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "0425399fb20fde198c5a",
+    "url": "/ReactPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4abe77dd10957fb13363",
+    "url": "/ReactPortfolio/static/js/2.f0626e77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.f0626e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0425399fb20fde198c5a",
+    "url": "/ReactPortfolio/static/js/main.43273edb.chunk.js"
+  },
+  {
+    "revision": "d09eb4ae4f588863f0c6",
+    "url": "/ReactPortfolio/static/js/runtime-main.722b3ff4.js"
+  }
+]);
